5c69fb47817c1e1d6f053a5a5730a7fd310eed5b
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-03-15 18:54:03 +0100

    feat: add main content card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,29 @@
 @section('title', 'COMICS - HOME')
 
 @section('main-content')
-<main>
-    
+<main class="home">
+    <div class="container">
+        <h1 class="text-uppercase">current series</h1>
+        
+        <div class="row gy-5">
+            @foreach ($comics as $comic)
+            <div class="col-2">
+                <div class="card-custom">
+                    <div class="img-container">
+                        <img src="{{$comic['thumb']}}" alt="">
+                    </div>
+                    <h3 class="mt-3 text-uppercase">{{$comic['series']}}</h3>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+        <div class="row justify-content-center mt-5">
+            <div class="col-2">
+                <div class="btn-custom text-center text-uppercase">load more</div>
+            </div>
+        </div>
+    </div>
 </main>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $menus = config('db.menus');
-    return view('home', compact('menus'));
+    $comics = config('db.comics');
+    return view('home', compact('menus', 'comics'));
 });
